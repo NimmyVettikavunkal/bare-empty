6ca8ba530e0a8f4ef77b4601ce2cde14fe9a81b3
user <example@gmail.com>
2023-10-16 13:18:44 +0000

    constructor ,getter and setter generated

--- src/main/java/com/example/sampledemo/model/Person.java
+++ src/main/java/com/example/sampledemo/model/Person.java
@@ -6,4 +6,31 @@ public class Person {
     private String name;
 
     private int age;
+
+    public Person() {
+    }
+
+    public Long getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(Long personId) {
+        this.personId = personId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
