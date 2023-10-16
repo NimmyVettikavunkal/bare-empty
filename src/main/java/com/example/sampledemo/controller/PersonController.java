8062cadb44cb7964eff9ed35d53ea4274230c95e
user <example@gmail.com>
2023-10-16 15:02:51 +0000

    Autowired annotation added

--- src/main/java/com/example/sampledemo/controller/PersonController.java
+++ src/main/java/com/example/sampledemo/controller/PersonController.java
@@ -1,5 +1,6 @@
 package com.example.sampledemo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,7 @@ import java.util.List;
 
 @RestController
 public class PersonController {
-
+//@Autowired
 
     }
 
